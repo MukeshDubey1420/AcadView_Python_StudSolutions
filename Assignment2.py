9cbfa42a2fcaf5638a3c56951a49e3c3cea39958
MukeshDubey1420 <mukeshdubey1420@gmail.com>
2018-06-28 16:46:41 +0530

    assignment2 is done ..

---
+++ Assignment2.py
@@ -0,0 +1,24 @@
+print ("My name is Mukesh Dubey")
+print ("Acad"+ "view")
+x=input("Enter a number : ")
+y=input("Enter a decimal value : ")
+z=input ("Enter a name : ")
+print ("Number entered is ",x)
+print ("Decimal value entered is ",y)
+print ("Name entered is ",z)
+print ("Let's get started")
+s="Acadview"
+course="Python"
+fees=5000
+print (s)
+print (course)
+print (fees)
+name="Tony Stark"
+salary=1000000
+print ('%s"%d'%(name,salary))
+
+
+
+
+
+
