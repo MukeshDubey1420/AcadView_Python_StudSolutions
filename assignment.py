874bf33f7adf0b98bd72e4f7046d7db360551e8a
MukeshDubey1420 <mukeshdubey1420@gmail.com>
2018-06-28 16:44:50 +0530

    assignment1 is done ..

---
+++ assignment.py
@@ -0,0 +1,31 @@
+#Question 1
+print ("Hello", "world")
+print ("Hello", "world", sep="-")
+print ("Hello world", end="")
+
+#Question2
+print("Acad"+"View")
+print("Mukesh"+"Dubey")
+
+#Question3
+x = input("enter Your message : ")
+print(x)
+y = input("enter Your message : ")
+print(y)
+z = input("enter Your message : ")
+print(z)
+
+#Question4
+print("Let\'s","get","started", sep=" ")
+
+#Question5
+s="Acadview"
+course = "Python"
+fees=5000
+print('%s %s %d' % (s, course, fees))
+
+#Question6
+print("Let\'s","do","some","interesting","exercise",":")
+name="Mukesh Dubey"
+salary=1000000
+print('%s %d '%(name ,salary))
