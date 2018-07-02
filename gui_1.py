0ea8db2b444e80c3af61ddfa5ba41467c676b889
MukeshDubey1420 <mukeshdubey1420@gmail.com>
2018-07-02 22:45:01 +0530

    gui 1 is updated

--- gui_1.py
+++ gui_1.py
@@ -46,16 +46,16 @@ w.pack()
 master.mainloop()
 
 #   Entry
-# from tkinter import *
-#
-# master =Tk()
-# Label(master,text='First name').grid(row=0)
-# Label(master,text='Second name').grid(row=1)
-# e1 = Entry(master)
-# e2 = Entry(master)
-# e1.grid(row=0,column=1)
-# e2.grid(row=1,column=1)
-# mainloop()
+from tkinter import *
+
+master =Tk()
+Label(master,text='First name').grid(row=0)
+Label(master,text='Second name').grid(row=1)
+e1 = Entry(master)
+e2 = Entry(master)
+e1.grid(row=0,column=1)
+e2.grid(row=1,column=1)
+mainloop()
 
 
 
