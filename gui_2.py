67f158223f99a5e698fe3e881d18041a6d9a6274
MukeshDubey1420 <mukeshdubey1420@gmail.com>
2018-07-04 11:11:40 +0530

    theory is updated

--- gui_2.py
+++ gui_2.py
@@ -97,20 +97,20 @@
 # top.mainloop()
 
 #SpinBox
-# from tkinter import *
-# master = Tk()
-# w = Spinbox(master, from_ = 0, to = 10)
-# w.pack()
-# mainloop()
+from tkinter import *
+master = Tk()
+w = Spinbox(master, from_ = 0, to = 10)
+w.pack()
+mainloop()
 
 #Paned Window
-from tkinter import *
-m1 = PanedWindow()
-m1.pack(fill = BOTH, expand = 1)
-left = Entry(m1, bd = 5)
-m1.add(left)
-m2 = PanedWindow(m1, orient = VERTICAL)
-m1.add(m2)
-top = Scale( m2, orient = HORIZONTAL)
-m2.add(top)
-mainloop()
+# from tkinter import *
+# m1 = PanedWindow()
+# m1.pack(fill = BOTH, expand = 1)
+# left = Entry(m1, bd = 5)
+# m1.add(left)
+# m2 = PanedWindow(m1, orient = VERTICAL)
+# m1.add(m2)
+# top = Scale( m2, orient = HORIZONTAL)
+# m2.add(top)
+# mainloop()
